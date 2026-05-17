353f1b39d96a12ffe2a91ee804dc88ae53cdb760
XyaraM <georgelangerneira@proton.me>
2026-05-17 02:10:26 -0400

    Added a note on install.py

--- install.py
+++ install.py
@@ -15,9 +15,15 @@ def checkIfVenv():
 
 if pyVer.startswith("3"):
     if checkIfVenv():
-        subprocess.run("venv/bin/python3 start.py", shell=True)
+        print(
+            "NOTE: The install script will only run the interaction mode, to run the CLI mode Use: venv/bin/python start.py\n"
+        )
+        subprocess.run("venv/bin/python3 start.py interact", shell=True)
     else:
         subprocess.run("python -m venv venv", shell=True)
         subprocess.run("venv/bin/pip install -r requirements.txt", shell=True)
+        print(
+            "NOTE: The install script will only run the interaction mode, to run the CLI mode Use: venv/bin/python start.py\n"
+        )
 else:
     print("Python 3 is needed")
